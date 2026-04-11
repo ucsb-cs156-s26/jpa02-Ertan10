cff9b3ea92c4e6e904e2684ce4d552d3eb9b1cdd
Ertan <165966424+Ertan10@users.noreply.github.com>
2026-04-10 22:06:00 -0700

    Update DeveloperTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -26,7 +26,7 @@ public class DeveloperTest {
 
     @Test
     public void getGithubId_returns_correct_githubId() {
-        assertEquals("15", Developer.getGithubId());
+        assertEquals("Ertan10", Developer.getGithubId());
     }
     
     @Test
