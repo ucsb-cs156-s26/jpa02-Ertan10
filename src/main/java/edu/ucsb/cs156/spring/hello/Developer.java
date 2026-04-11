b28f2dbb66bed352f97a89c1cf2001a959f474dd
Ertan <165966424+Ertan10@users.noreply.github.com>
2026-04-10 20:07:41 -0700

    Update Developer.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -41,12 +41,12 @@ public class Developer {
     public static Team getTeam() {
         // TODO: Change this to your team name
         Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        team.addMember("Vanessa");
+        team.addMember("Vincent");
+        team.addMember("Mehmet");
+        team.addMember("Annie");
+        team.addMember("Tanvi");
+        team.addMember("Erik");
         return team;
     }
 }
