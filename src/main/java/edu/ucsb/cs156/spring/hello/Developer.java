31a6ee63b706596fe42992e8b073cbefea14d8ee
Ertan <165966424+Ertan10@users.noreply.github.com>
2026-04-10 21:37:46 -0700

    Update Developer.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -1,41 +1,27 @@
 package edu.ucsb.cs156.spring.hello;
 
-/**
- * A class with static methods to provide information about the developer.
- */
-
 public class Developer {
 
-    // This class is not meant to be instantiated
-    // so we make the constructor private
+ 
 
     private Developer() {}
     
-    /**
-     * Get the name of the developer
-     */
+  
 
     public static String getName() {
      
-        // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
-        // i.e. your first name, or your first and initial of last name
+       
 
         return "Mehmet";
     }
 
-    /**
-     * Get the github id of the developer
-     * @return github id of the developer
-     */
+ 
 
     public static String getGithubId() {
         return "A1C5E26";
     }
 
-    /**
-     * Get the developers team
-     * @return developers team as a Java object
-     */
+   
     
     public static Team getTeam() {
         Team team = new Team("s26-04");
