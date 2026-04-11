193749511ffc343494f877a3eb22382405338fa5
Ertan <165966424+Ertan10@users.noreply.github.com>
2026-04-10 20:57:02 -0700

    Update Team.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Team.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Team.java
@@ -80,10 +80,10 @@ public class Team {
     /*
      * Get a string representation of the team
      */
-    // @Override
-    // public String toString() {
-    //     return "Team(name=" + this.name + ", members=" + this.members + ")";
-    // }
+    @Override
+    public String toString() {
+        return "Team(name=" + this.name + ", members=" + this.members + ")";
+    }
 
     /**
      * Get a hash code for the team
